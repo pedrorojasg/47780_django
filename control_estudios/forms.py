3faf88e2cd5af2227233d9c17115841abdadc442
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-11-02 19:15:40 -0400

    Agregado formulario usando django forms

---
+++ control_estudios/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class CursoFormulario(forms.Form):
+    nombre = forms.CharField(required=True, max_length=64)
+    comision = forms.IntegerField(required=True, max_value=80000)
