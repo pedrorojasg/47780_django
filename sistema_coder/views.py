05fbd96d22fa579ed80b24feb6c4ebd4c853dcd0
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-11-02 18:17:06 -0400

    Comentadas view no usadas

--- sistema_coder/views.py
+++ sistema_coder/views.py
@@ -4,25 +4,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def saludar(request):
-    saludo = "Hola querido usuario/a"
-    respuesta_http = HttpResponse(saludo)
-    return respuesta_http
+# No en uso
+# def saludar(request):
+#     saludo = "Hola querido usuario/a"
+#     respuesta_http = HttpResponse(saludo)
+#     return respuesta_http
 
 
-def saludar_con_fecha(request):
-    hoy = datetime.now()
-    saludo = f"Hola querido usuario/a, fecha: {hoy.day}/{hoy.month}"
-    respuesta_http = HttpResponse(saludo)
-    return respuesta_http
+# def saludar_con_fecha(request):
+#     hoy = datetime.now()
+#     saludo = f"Hola querido usuario/a, fecha: {hoy.day}/{hoy.month}"
+#     respuesta_http = HttpResponse(saludo)
+#     return respuesta_http
 
 
 def saludar_con_html(request):
-    contexto = {
-        "profesor": "Pedro",
-        "tutores": ["Mariano", "Ruben", "Luciano"],
-        "comision": 47780,
-    }
+    contexto = {}
     http_response = render(
         request=request,
         template_name='inicio.html',
