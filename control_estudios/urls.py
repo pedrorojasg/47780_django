efd4ba6f19ef5cd44f2828a8e32349bfa15e9126
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-11-02 19:51:28 -0400

    Busqueda de cursos

--- control_estudios/urls.py
+++ control_estudios/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from control_estudios.views import listar_estudiantes, listar_cursos, crear_curso
+from control_estudios.views import (
+    listar_estudiantes, listar_cursos, crear_curso, buscar_cursos
+)
 
 # Son las URLS de la app control_estudios
 urlpatterns = [
     path("estudiantes/", listar_estudiantes, name="lista_estudiantes"),
     path("cursos/", listar_cursos, name="lista_cursos"),
     path("crear-curso/", crear_curso, name="crear_curso"),
+    path("buscar-cursos/", buscar_cursos, name="buscar_cursos"),
 ]
