039154c5824c07f99a5b445dba363594d54b8f58
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-11-14 18:23:26 -0400

    Editar usuario

--- perfiles/urls.py
+++ perfiles/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from perfiles.views import registro, login_view, CustomLogoutView
+from perfiles.views import registro, login_view, CustomLogoutView, MiPerfilUpdateView
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('registro/', registro, name="registro"),
     path('login/', login_view, name="login"),
     path('logout/', CustomLogoutView.as_view(), name="logout"),
+    path('editar-mi-perfil/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
 ]
